04ff9393c9bd0e02054c2432b2c11d9580349b50
ezSan <ezequiel.sanchez.7391@gmail.com>
2023-05-22 17:48:25 -0300

    style: nav bar with flex

--- app/components/header/navBar/NavBar.js
+++ app/components/header/navBar/NavBar.js
@@ -1,36 +1,26 @@
 import React from "react";
 import styles from "../../../scss/globals.module.scss";
-import Nav from 'react-bootstrap/Nav';
+import Nav from "react-bootstrap/Nav";
 
 const { styledNavBar } = styles;
 
 export default function NavBar() {
   return (
-    <div className={styledNavBar}>
-      <Nav
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-      >
+    <Nav className={styledNavBar}>
+      <Nav.Item>
+        <Nav.Link href="#about">SOBRE MI</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#tech">TECNOLOGÍAS</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#projects">PROYECTOS</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/home">SOBRE MI</Nav.Link>
+          <Nav.Link href="#contact">CONTACTO</Nav.Link>
         </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">TECNOLOGÍAS</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">PROYECTOS</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">CONTACTO</Nav.Link>
-        </Nav.Item>
-        </Nav.Item>
-      </Nav>
-
-      {/*       <a id="#" href="#about">SOBRE MI</a>
-      <a id="#" href="#tech">TECNOLOGÍAS</a>
-      <a id="#" href="#projects">PROYECTOS</a>
-      <a id="#" href="#contact">CONTACTO</a> */}
-    </div>
+      </Nav.Item>
+    </Nav>
   );
 }
