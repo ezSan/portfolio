46fcf056c65ab0e083bc77bca02a341dff0e5281
ezSan <ezequiel.sanchez.7391@gmail.com>
2023-06-26 18:10:01 -0300

    lastChanges-Re Styled all page

--- app/components/header/navBar/NavBar.js
+++ app/components/header/navBar/NavBar.js
@@ -1,7 +1,21 @@
 import React from "react";
 import styles from "../../../scss/globals.module.scss";
+import { motion, useAnimation } from "framer-motion";
 
 const { styledNavBar, listItems, navBarOpen } = styles;
+
+const smoothScrollAnimation = {
+  duration: 0.8, // Duración de la animación en segundos
+  ease: [0.43, 0.13, 0.23, 0.96], // Curva de aceleración personalizada
+};
+
+const handleSmoothScroll = (sectionId) => {
+  const element = document.getElementById(sectionId);
+  element.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+};
 
 export default function NavBar(props) {
   const { isOpen } = props;
@@ -9,18 +23,38 @@ export default function NavBar(props) {
   return (
     <nav className={isOpen ? navBarOpen : styledNavBar}>
       <ul className={listItems}>
-        <a href="#about">
-          <li>SOBRE MI</li>
-        </a>
-        <a href="#tech">
-          <li>TECNOLOGÍAS</li>
-        </a>
-        <a href="#projects">
-          <li>PROYECTOS</li>
-        </a>
-        <a href="#contact">
-          <li>CONTACTO</li>
-        </a>
+        <motion.a
+          onClick={() => handleSmoothScroll("about")}
+          className="nav-link"
+          whileTap={{ scale: 0.9 }}
+          transition={smoothScrollAnimation}
+        >
+          Sobre mi
+        </motion.a>
+        <motion.a
+          onClick={() => handleSmoothScroll("tech")}
+          className="nav-link"
+          whileTap={{ scale: 0.9 }}
+          transition={smoothScrollAnimation}
+        >
+          Tecnologías
+        </motion.a>
+        <motion.a
+          onClick={() => handleSmoothScroll("projects")}
+          className="nav-link"
+          whileTap={{ scale: 0.9 }}
+          transition={smoothScrollAnimation}
+        >
+          Projectos
+        </motion.a>
+        <motion.a
+          onClick={() => handleSmoothScroll("contact")}
+          className="nav-link"
+          whileTap={{ scale: 0.9 }}
+          transition={smoothScrollAnimation}
+        >
+          Contacto
+        </motion.a>
       </ul>
     </nav>
   );
--- app/components/projects/ProjectCard.js
+++ app/components/projects/ProjectCard.js
@@ -14,20 +14,18 @@ const {
 
 export default function ProjectCard(props) {
   return (
-    <Link href='/gifos'>
-      <div className={project_card}>
-        <Image
-          alt={props.nombre}
-          src={props.image}
-          className={project_card__image}
-          width={150}
-          height={150}
-        />
-        <div className={project_card__content}>
-          <h3 className={project_card__title}>{props.nombre}</h3>
-          <p className={project_card__description}>{props.desc}</p>
-        </div>
+    <div className={project_card}>
+      <Image
+        alt={props.nombre}
+        src={props.image}
+        className={project_card__image}
+        width={150}
+        height={150}
+      />
+      <div className={project_card__content}>
+        <h3 className={project_card__title}>{props.nombre}</h3>
+        <p className={project_card__description}>{props.desc}</p>
       </div>
-    </Link>
+    </div>
   );
 }
