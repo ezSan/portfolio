4266cbc6812bffad478ed691615f80dcf6517a2b
ezSan <ezequiel.sanchez.7391@gmail.com>
2023-07-04 15:51:29 -0300

    project cards styled and responsive

--- app/components/projects/ProjectCard.js
+++ app/components/projects/ProjectCard.js
@@ -1,32 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../scss/globals.module.scss";
 import { GitHub } from "iconoir-react";
 import Image from "next/image";
 import Link from "next/link";
 
-
 const {
-  project_card__image,
-  project_card__title,
-  project_card__content,
-  project_card__description,
-  project_card,
+  projectCard,
+  image,
+  title,
+  technologies,
+  span,
+  description,
+  buttons,
+  button,
 } = styles;
 
 export default function ProjectCard(props) {
+  const [tecn, setTecnologías] = useState([props.tecnologias[0]]);
+
   return (
-   <div>
-
-   
-
-   </div>
+    <div className={projectCard}>
+      <Image
+        src={props.image}
+        alt={props.nombre}
+        className={image}
+        width={150}
+        height={150}
+      />
+      <h2 className={title}>{props.nombre}</h2>
+      <div className={technologies}>
+        {tecn.map((tecno, index) =>
+          tecno.map((p) => <p className={span}>{p}</p>)
+        )}
+      </div>
+      <p className={description}>{props.desc}</p>
+      <div className={buttons}>
+        {props.github && (
+          <a
+            href={props.github}
+            className={button}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        )}
+        {props.deploy && (
+          <a
+            href={props.deploy}
+            className={button}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Deploy
+          </a>
+        )}
+      </div>
+    </div>
   );
 }
 
-
-
 {
-/*   <div className={project_card}>
+  /*   <div className={project_card}>
   <Image
     alt={props.nombre}
     src={props.image}
