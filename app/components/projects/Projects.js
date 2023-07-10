5b053845b47ed5000b1d73323d702cb0d4e41258
ezSan <ezequiel.sanchez.7391@gmail.com>
2023-07-10 14:43:40 -0300

    fixed animation

--- app/components/projects/Projects.js
+++ app/components/projects/Projects.js
@@ -9,10 +9,9 @@ import { useInView } from "react-intersection-observer";
 const { projects, styledProjectsContainer, title } = styles;
 
 const arrProy = proyectos.proyects;
-console.log(arrProy);
 
 export default function Projects() {
-  const [ref, inView] = useInView();
+  const [ref, inView] = useInView({ triggerOnce: true });
   const controls = useAnimation();
 
   useEffect(() => {
@@ -23,26 +22,30 @@ export default function Projects() {
 
   const cardVariants = {
     hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5, ease: "easeOut" },
+    },
   };
+
   const [arrPro, setProyectos] = useState(arrProy);
 
   return (
     <section className={projects} id="projects">
       <h3 className={title}>PROYECTOS</h3>
-      <div className={styledProjectsContainer}>
+      <div className={styledProjectsContainer} ref={ref}>
         {arrPro.map((py, index) => {
           return (
             <div key={index}>
               <AnimatePresence>
                 <motion.div
-                  ref={ref}
                   initial="hidden"
                   animate={controls}
                   variants={cardVariants}
+                  exit={{ opacity: 0, y: -50 }}
                 >
                   <ProjectCard
-                    
                     image={py.image}
                     nombre={py.nombre}
                     desc={py.desc}
